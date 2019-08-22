8a6659c4fa1265f66b715842f72e49bfa6b7155b
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-22 20:27:27 +0900

    .

--- app/src/main/java/com/example/flut_fluss/view/activity/MainActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.flut_fluss.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.flut_fluss.R;
 import com.example.flut_fluss.databinding.MainActivityBinding;
+import com.example.flut_fluss.view.fragment.RemittanceFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        initData();
+
+        event();
+    }
+
+    private void initData() {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.main_activity_fragment, new RemittanceFragment());
+        fragmentTransaction.commit();
+    }
+
+    private void event() {
+
     }
 }
