4a5bffa9b2c21807ccc063a6becdca10db77ee6f
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-23 20:39:50 +0900

    Update RemittanceFragment.java

--- app/src/main/java/com/example/flut_fluss/view/fragment/RemittanceFragment.java
+++ app/src/main/java/com/example/flut_fluss/view/fragment/RemittanceFragment.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.databinding.RemittanceFragmentBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +24,7 @@ import com.example.flut_fluss.R;
  */
 public class RemittanceFragment extends Fragment {
 
-    
+    RemittanceFragmentBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -70,7 +72,9 @@ public class RemittanceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.remittance_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.remittance_fragment, container, false);
+
+        return binding.getRoot();
     }
 
     @Override
