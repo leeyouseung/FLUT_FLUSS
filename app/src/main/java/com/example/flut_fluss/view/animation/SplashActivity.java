2e58a451bf95f2cb542e189a2e0296c9433a8b92
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-23 13:46:07 +0900

    .

--- app/src/main/java/com/example/flut_fluss/view/animation/SplashActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/animation/SplashActivity.java
@@ -7,17 +7,22 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.databinding.SplashActivityBinding;
 import com.example.flut_fluss.view.activity.LoginActivity;
 
 @SuppressLint("Registered")
 public class SplashActivity extends AppCompatActivity {
+
+    SplashActivityBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        initData();
 
         Animation hold = AnimationUtils.loadAnimation(this, R.anim.splash_hold);
 
@@ -32,8 +37,8 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animation animation) {
 
-                binding.logo.clearAnimation();
-                binding.logo.startAnimation(scale);
+                binding.animationLogo.clearAnimation();
+                binding.animationLogo.startAnimation(scale);
             }
 
             @Override
@@ -51,7 +56,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animation animation) {
 
-                binding.logo.clearAnimation();
+                binding.animationLogo.clearAnimation();
 
                 Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
@@ -65,6 +70,11 @@ public class SplashActivity extends AppCompatActivity {
             }
         });
 
-        binding.logo.startAnimation(hold);
+        binding.animationLogo.startAnimation(hold);
+    }
+
+    private void initData() {
+
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
     }
 }
