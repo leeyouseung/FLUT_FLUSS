dc0f3caf31c2a27eaa9be011df36741d5ffb1c8c
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-23 14:18:06 +0900

    create RegisterActivity & LoginActivity Fixed

--- app/src/main/java/com/example/flut_fluss/view/activity/LoginActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/LoginActivity.java
@@ -3,7 +3,11 @@ package com.example.flut_fluss.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.Toast;
 
 import com.example.flut_fluss.R;
 import com.example.flut_fluss.databinding.LoginActivityBinding;
@@ -12,15 +16,85 @@ public class LoginActivity extends AppCompatActivity {
 
     LoginActivityBinding binding;
 
+    private boolean checkBlind = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.login_activity);
 
         initData();
+
+        event();
     }
 
     private void initData() {
 
+    }
+
+    private void event() {
+
+        clickEvent();
+    }
+
+    private void clickEvent() {
+
+        clickPasswordClearButton();
+        clickBlindButton();
+        clickLoginButton();
+    }
+
+    private void clickPasswordClearButton() {
+
+        binding.clear.setOnClickListener(v -> binding.passwordText.setText(""));
+    }
+
+    private void clickBlindButton() {
+
+        binding.blind.setOnClickListener(v -> {
+
+            if (checkBlind) {
+
+                binding.blind.setImageResource(R.drawable.ic_blind);
+                binding.passwordText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+                checkBlind = false;
+            }
+            else {
+
+                binding.blind.setImageResource(R.drawable.ic_blind_check);
+                binding.passwordText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+                checkBlind = true;
+            }
+        });
+    }
+
+    private void clickLoginButton() {
+
+        binding.loginButton.setOnClickListener(v -> {
+
+            if (binding.idText.getText().toString().isEmpty()) {
+
+                Toast.makeText(this,"아이디를 입력해주세요",Toast.LENGTH_SHORT).show();
+
+                return;
+            }
+            if (binding.passwordText.getText().toString().isEmpty()) {
+
+                Toast.makeText(this,"비밀번호를 입력해주세요",Toast.LENGTH_SHORT).show();
+
+                return;
+            }
+//            loginViewModel.login(
+//                    new LoginRequest(binding.idText.getText().toString(),
+//                            binding.passwordText.getText().toString()));
+        });
+
+        binding.registerButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
