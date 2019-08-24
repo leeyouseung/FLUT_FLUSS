93ea8c29480cec74bf8c01875cd0b5a93f5d3537
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-24 16:41:20 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/MainActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 나르샤 끝나고 할 일 :
         // 돈 입력 했을 때, 하단 메뉴바 사라지게 만들고 더치페이 보내기 만들기   70%
-        // 돈 입력 기능 구현.                                               80%
+        // 돈 입력 기능 구현.                                               100%
         // qr 코드 구현.                                                    0%
     }
 
