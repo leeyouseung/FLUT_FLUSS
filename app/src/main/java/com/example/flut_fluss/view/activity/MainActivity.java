a1f40e069bc63d02f3527020a59ee04dee0e89ee
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-25 19:34:42 +0900

    save spot

--- app/src/main/java/com/example/flut_fluss/view/activity/MainActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private void initData() {
 
         LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
+
         assert layoutInflater != null;
 
         View view = layoutInflater.inflate(R.layout.remittance_fragment, null);
