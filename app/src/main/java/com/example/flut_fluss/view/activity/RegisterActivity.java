50d1c3093f61eaa69e9cd866deed2add7a649063
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-25 20:34:01 +0900

    Update RegisterActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/RegisterActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/RegisterActivity.java
@@ -1,16 +1,20 @@
 package com.example.flut_fluss.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.databinding.RegisterActivityBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private RegisterActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_activity);
+        binding = DataBindingUtil.setContentView(this, R.layout.register_activity);
     }
 }
