229d646cc4f0b892dab1a13e8a531f11bad36a71
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-26 19:40:52 +0900

    set binding

--- app/src/main/java/com/example/flut_fluss/view/activity/AccountActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/AccountActivity.java
@@ -5,14 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.databinding.AccountActivityBinding;
 
 public class AccountActivity extends AppCompatActivity {
 
-    
+    AccountActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.account_activity);
     }
 }
