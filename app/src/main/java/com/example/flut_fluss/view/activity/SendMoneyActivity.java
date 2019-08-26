ac858791455e710830fedee1256d21e2951c9b6e
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-26 13:08:09 +0900

    Update SendMoneyActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/SendMoneyActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/SendMoneyActivity.java
@@ -1,16 +1,32 @@
 package com.example.flut_fluss.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.databinding.SendMoneyActivityBinding;
 
 public class SendMoneyActivity extends AppCompatActivity {
+
+    SendMoneyActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.send_money_activity);
+        binding = DataBindingUtil.setContentView(this, R.layout.send_money_activity);
+
+        initData();
+
+        event();
+    }
+
+    private void initData() {
+
+    }
+
+    private void event() {
+
     }
 }
