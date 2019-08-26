8098926fed7877407d7ca0e7b56425c30f0ecd9e
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-26 13:06:53 +0900

    Update SendMoneyActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/SendMoneyActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/SendMoneyActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.flut_fluss.R;
+
 public class SendMoneyActivity extends AppCompatActivity {
 
     @Override
