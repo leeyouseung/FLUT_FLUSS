d3abd9f6125b2a6770eca06e9a8afd88279bc386
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-26 15:08:55 +0900

    Update ShowMoneyListAdapter.java

--- app/src/main/java/com/example/flut_fluss/widget/recyclerview/adapter/ShowMoneyListAdapter.java
+++ app/src/main/java/com/example/flut_fluss/widget/recyclerview/adapter/ShowMoneyListAdapter.java
@@ -1,4 +1,27 @@
 package com.example.flut_fluss.widget.recyclerview.adapter;
 
-public class ShowMoneyListAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ShowMoneyListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return 0;
+    }
 }
