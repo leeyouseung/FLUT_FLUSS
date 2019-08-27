38e3a99dd471c1a0d8afd63f862144c7f6664d80
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-27 17:19:48 +0900

    Update ShowMoneyListFragment.java

--- app/src/main/java/com/example/flut_fluss/view/fragment/ShowMoneyListFragment.java
+++ app/src/main/java/com/example/flut_fluss/view/fragment/ShowMoneyListFragment.java
@@ -2,70 +2,18 @@ package com.example.flut_fluss.view.fragment;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.base.BaseFragment;
 import com.example.flut_fluss.databinding.ShowMoneyListFragmentBinding;
 
 
-public class ShowMoneyListFragment extends Fragment {
-
-    private ShowMoneyListFragmentBinding binding;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public ShowMoneyListFragment() {
-        // Required empty public constructor
-    }
-
-    // TODO: Rename and change types and number of parameters
-    public static ShowMoneyListFragment newInstance(String param1, String param2) {
-        ShowMoneyListFragment fragment = new ShowMoneyListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.show_money_list_fragment, container, false);
-
-        return binding.getRoot();
-    }
+public class ShowMoneyListFragment extends BaseFragment<ShowMoneyListFragmentBinding> {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -136,20 +84,9 @@ public class ShowMoneyListFragment extends Fragment {
         });
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    protected int layoutId() {
+
+        return R.layout.show_money_list_fragment;
     }
 }
