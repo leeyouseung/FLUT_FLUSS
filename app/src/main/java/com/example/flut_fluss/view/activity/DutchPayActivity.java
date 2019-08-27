0d571aff8182383db2b11af3417b39efa14327ef
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-27 10:52:19 +0900

    Update DutchPayActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/DutchPayActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/DutchPayActivity.java
@@ -1,16 +1,20 @@
 package com.example.flut_fluss.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.databinding.DutchPayActivityBinding;
 
 public class DutchPayActivity extends AppCompatActivity {
+
+    DutchPayActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dutch_pay_activity);
+        binding = DataBindingUtil.setContentView(this ,R.layout.dutch_pay_activity);
     }
 }
