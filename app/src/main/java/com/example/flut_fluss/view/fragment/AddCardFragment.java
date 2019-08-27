43e255d8a1617885a03b730a5bd23290dd43e4d8
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-27 17:19:03 +0900

    Update AddCardFragment.java

--- app/src/main/java/com/example/flut_fluss/view/fragment/AddCardFragment.java
+++ app/src/main/java/com/example/flut_fluss/view/fragment/AddCardFragment.java
@@ -1,81 +1,14 @@
 package com.example.flut_fluss.view.fragment;
 
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.flut_fluss.R;
+import com.example.flut_fluss.base.BaseFragment;
 import com.example.flut_fluss.databinding.AddCardFragmentBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddCardFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddCardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddCardFragment extends Fragment {
-
-    private AddCardFragmentBinding binding;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public AddCardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddCardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddCardFragment newInstance(String param1, String param2) {
-        AddCardFragment fragment = new AddCardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.add_card_fragment, container, false);
-
-        return binding.getRoot();
-    }
+public class AddCardFragment extends BaseFragment<AddCardFragmentBinding> {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -88,31 +21,9 @@ public class AddCardFragment extends Fragment {
 
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+    protected int layoutId() {
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return R.layout.add_card_fragment;
     }
 }
