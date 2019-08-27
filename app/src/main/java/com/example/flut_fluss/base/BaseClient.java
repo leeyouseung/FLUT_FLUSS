009339de1b6095606eebe55ee19591b7017d5cb0
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-27 16:58:04 +0900

    ....

--- app/src/main/java/com/example/flut_fluss/base/BaseClient.java
+++ app/src/main/java/com/example/flut_fluss/base/BaseClient.java
@@ -2,13 +2,14 @@ package com.example.flut_fluss.base;
 
 import android.util.Log;
 
+import com.example.flut_fluss.utils.Utils;
+
 import org.json.JSONObject;
-import org.techtown.betweenus_android.network.Response;
-import org.techtown.betweenus_android.utils.Utils;
 
 import java.util.Objects;
 
 import io.reactivex.functions.Function;
+import retrofit2.Response;
 
 public abstract class BaseClient<V> {
 
@@ -17,21 +18,26 @@ public abstract class BaseClient<V> {
     protected abstract Class api();
 
     public BaseClient() {
+
         api = (V) Utils.RETROFIT.create(api());
     }
 
     protected <T> Function<retrofit2.Response<Response<T>>, T> getResponseObjectsFunction() {
-        return response -> {
-                    if (!response.isSuccessful()) {
-                        JSONObject errorBody = new JSONObject(Objects
-                                .requireNonNull(
-                                        response.errorBody()).string());
-                        Log.e("message", errorBody.getString("message"));
-                        throw new Exception(errorBody.getString("message"));
-                    }
-                    Log.e("message", response.body().getStatus() + "");
-                    return (T) response.body().getData();
-                };
-    }
 
+        return response -> {
+
+            if (!response.isSuccessful()) {
+
+                JSONObject errorBody = new JSONObject(Objects.requireNonNull(response.errorBody()).string());
+
+                Log.e("message", errorBody.getString("message"));
+
+                throw new Exception(errorBody.getString("message"));
+            }
+
+            Log.e("message", response.body().getStatus() + "");
+
+            return (T) response.body().getData();
+        };
+    }
 }
---
+++ app/src/main/java/com/example/flut_fluss/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.example.flut_fluss.utils;
+
+public final class Constants {
+
+    public static final String MAIN_HOST = "";
+}
