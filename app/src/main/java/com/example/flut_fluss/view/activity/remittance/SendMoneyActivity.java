3acd9db9195415c73b7003e8bcdcb25ed423dd49
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-28 20:00:00 +0900

    set

--- app/src/main/java/com/example/flut_fluss/view/activity/remittance/SendMoneyActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/remittance/SendMoneyActivity.java
@@ -3,12 +3,15 @@ package com.example.flut_fluss.view.activity.remittance;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.flut_fluss.R;
 import com.example.flut_fluss.base.BaseActivity;
 import com.example.flut_fluss.databinding.SendMoneyActivityBinding;
 
 public class SendMoneyActivity extends BaseActivity<SendMoneyActivityBinding> {
+
+    boolean openSetLayout = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,28 @@ public class SendMoneyActivity extends BaseActivity<SendMoneyActivityBinding> {
 
     private void event() {
 
+        clickEvent();
+    }
+
+    private void clickEvent() {
+
+        binding.myAccountListLayout.setOnClickListener(v -> {
+
+            openSetLayout = true;
+
+            if(openSetLayout) {
+
+                binding.accountLayout.setVisibility(View.VISIBLE);
+
+                binding.cardLayout.setVisibility(View.VISIBLE);
+            }
+            else {
+
+                binding.accountLayout.setVisibility(View.INVISIBLE);
+
+                binding.cardLayout.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
     @Override
