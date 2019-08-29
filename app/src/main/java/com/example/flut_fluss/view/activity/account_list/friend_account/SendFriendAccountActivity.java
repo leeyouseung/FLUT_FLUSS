7702ceb26a77d22fd3c88eb44e87e4158017e762
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-29 13:21:13 +0900

    Update SendFriendAccountActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/account_list/friend_account/SendFriendAccountActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/account_list/friend_account/SendFriendAccountActivity.java
@@ -1,6 +1,7 @@
 package com.example.flut_fluss.view.activity.account_list.friend_account;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.flut_fluss.R;
 import com.example.flut_fluss.base.BaseActivity;
@@ -12,6 +13,42 @@ public class SendFriendAccountActivity extends BaseActivity<SendFriendAccountAct
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        initData();
+
+        event();
+    }
+
+    private void initData() {
+
+    }
+
+    private void event() {
+
+        clickEvent();
+    }
+
+    private void clickEvent() {
+
+        clickMyAccountLayout();
+        clickMyBankLayout();
+    }
+
+    private void clickMyAccountLayout() {
+
+        binding.myAccountLayout.setOnClickListener(v -> {
+
+            binding.pqyCheck1.setVisibility(View.VISIBLE);
+            binding.pqyCheck2.setVisibility(View.INVISIBLE);
+        });
+    }
+
+    private void clickMyBankLayout() {
+
+        binding.myBankLayout.setOnClickListener(v -> {
+
+            binding.pqyCheck1.setVisibility(View.INVISIBLE);
+            binding.pqyCheck2.setVisibility(View.VISIBLE);
+        });
     }
 
     @Override
