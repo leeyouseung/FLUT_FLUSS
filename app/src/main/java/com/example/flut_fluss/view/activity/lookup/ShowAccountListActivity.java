c4b6a6a1effa87a8f11cf676c6b9f9e4212a02ff
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-29 20:55:00 +0900

    set

---
+++ app/src/main/java/com/example/flut_fluss/view/activity/lookup/ShowAccountListActivity.java
@@ -0,0 +1,38 @@
+package com.example.flut_fluss.view.activity.lookup;
+
+import android.os.Bundle;
+
+import com.example.flut_fluss.R;
+import com.example.flut_fluss.base.BaseActivity;
+import com.example.flut_fluss.databinding.ShowAccountListActivityBinding;
+
+public class ShowAccountListActivity extends BaseActivity<ShowAccountListActivityBinding> {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initData();
+
+        event();
+    }
+
+    private void initData() {
+
+    }
+
+    private void event() {
+
+        clickEvent();
+    }
+
+    private void clickEvent() {
+        
+    }
+
+    @Override
+    protected int layoutId() {
+
+        return R.layout.show_account_list_activity;
+    }
+}
