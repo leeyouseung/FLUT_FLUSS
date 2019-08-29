96cbeb322d5e98a7cd2a78ee54d5ee2e61315c98
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-29 15:12:09 +0900

    Update LookupSendMoneyActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/lookup/remittance/LookupSendMoneyActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/lookup/remittance/LookupSendMoneyActivity.java
@@ -1,12 +1,17 @@
 package com.example.flut_fluss.view.activity.lookup.remittance;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.flut_fluss.R;
 import com.example.flut_fluss.base.BaseActivity;
 import com.example.flut_fluss.databinding.LookupSendMoneyActivityBinding;
 
 public class LookupSendMoneyActivity extends BaseActivity<LookupSendMoneyActivityBinding> {
+
+    public String sendMoney = "0";
+
+    private boolean isFirstInput = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class LookupSendMoneyActivity extends BaseActivity<LookupSendMoneyActivit
 
     private void initData() {
 
+        binding.money.setText(String.valueOf(0));
     }
 
     private void event() {
@@ -26,8 +32,388 @@ public class LookupSendMoneyActivity extends BaseActivity<LookupSendMoneyActivit
         clickEvent();
     }
 
+    private void visibleDeleteCancelButton() {
+
+        binding.deleteButton.setVisibility(View.VISIBLE);
+        binding.cancelButton.setVisibility(View.VISIBLE);
+    }
+
+    private void invisibleDeleteCancelButton() {
+
+        binding.deleteButton.setVisibility(View.INVISIBLE);
+        binding.cancelButton.setVisibility(View.INVISIBLE);
+    }
+
     private void clickEvent() {
-        
+
+        clickMoneyNumber0();
+        clickMoneyNumber1();
+        clickMoneyNumber2();
+        clickMoneyNumber3();
+        clickMoneyNumber4();
+        clickMoneyNumber5();
+        clickMoneyNumber6();
+        clickMoneyNumber7();
+        clickMoneyNumber8();
+        clickMoneyNumber9();
+        clickCancelButton();
+        clickDeleteButton();
+    }
+
+    private void clickMoneyNumber0() {
+
+        binding.moneyNumber0.setOnClickListener(v -> {
+
+            if (isFirstInput && binding.money.getText().toString().equals("0")) {
+
+            }
+            else {
+
+                binding.money.append(String.valueOf(0));
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber1() {
+
+        binding.moneyNumber1.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(1));
+
+                isFirstInput = false;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(1));
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+
+    private void clickMoneyNumber2() {
+
+        binding.moneyNumber2.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(2));
+
+                isFirstInput = false;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(2));
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber3() {
+
+        binding.moneyNumber3.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(3));
+
+                isFirstInput = false;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(3));
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber4() {
+
+        binding.moneyNumber4.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(4));
+
+                isFirstInput = false;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(4));
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber5() {
+
+        binding.moneyNumber5.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(5));
+
+                isFirstInput = false;
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(5));
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber6() {
+
+        binding.moneyNumber6.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(6));
+
+                isFirstInput = false;
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(6));
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber7() {
+
+        binding.moneyNumber7.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(7));
+
+                isFirstInput = false;
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(7));
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber8() {
+
+        binding.moneyNumber8.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(8));
+
+                isFirstInput = false;
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(8));
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickMoneyNumber9() {
+
+        binding.moneyNumber9.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                binding.money.setText(String.valueOf(9));
+
+                isFirstInput = false;
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                setMoveMoneyCountTitle();
+
+                sendMoney = binding.money.getText().toString();
+            }
+            else {
+
+                binding.money.append(String.valueOf(9));
+
+                ;
+
+                visibleDeleteCancelButton();
+
+                sendMoney = binding.money.getText().toString();
+            }
+        });
+    }
+
+    private void clickCancelButton() {
+
+        binding.cancelButton.setOnClickListener(v -> {
+
+            binding.money.setText(String.valueOf(0));
+
+            isFirstInput = true;
+
+            invisibleDeleteCancelButton();
+
+            resetMoveMoneyCountTitle();
+        });
+    }
+
+    private void clickDeleteButton() {
+
+        binding.deleteButton.setOnClickListener(v -> {
+
+            if (isFirstInput) {
+
+                isFirstInput = true;
+
+                invisibleDeleteCancelButton();
+
+                resetMoveMoneyCountTitle();
+            }
+            else {
+
+                if (binding.money.getText().toString().length() == 1 && binding.money.getText().toString().equals("0")) {
+
+                    binding.money.setText(String.valueOf(0));
+
+                    isFirstInput = true;
+
+                    invisibleDeleteCancelButton();
+
+                    resetMoveMoneyCountTitle();
+                }
+                else if (binding.money.getText().toString().length() == 1 && !(binding.money.getText().toString().equals("0"))) {
+
+                    binding.money.setText(String.valueOf(0));
+
+                    isFirstInput = true;
+
+                    invisibleDeleteCancelButton();
+
+                    resetMoveMoneyCountTitle();
+                }
+                else {
+
+                    String moneySet = binding.money.getText().toString().substring(0, binding.money.getText().toString().length() - 1);
+
+                    binding.money.setText(moneySet);
+
+                    isFirstInput = false;
+
+                    sendMoney = binding.money.getText().toString();
+                }
+            }
+        });
+    }
+
+    private void setMoveMoneyCountTitle() {
+
+        binding.money.setX(binding.money.getX() + 42);
+        binding.moneyTitle.setX(binding.moneyTitle.getX() + 42);
+    }
+
+    private void resetMoveMoneyCountTitle() {
+
+        binding.money.setX(binding.money.getX() - 42);
+        binding.moneyTitle.setX(binding.moneyTitle.getX() - 42);
     }
 
     @Override
