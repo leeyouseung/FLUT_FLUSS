c5a1135f71731a956ba7eb25593a0fb2af72e52c
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-30 09:44:22 +0900

    Update ShowAccountListActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/lookup/ShowAccountListActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/lookup/ShowAccountListActivity.java
@@ -1,10 +1,12 @@
 package com.example.flut_fluss.view.activity.lookup;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.flut_fluss.R;
 import com.example.flut_fluss.base.BaseActivity;
 import com.example.flut_fluss.databinding.ShowAccountListActivityBinding;
+import com.example.flut_fluss.view.activity.lookup.account.remittance.LookupSendMoneyActivity;
 
 public class ShowAccountListActivity extends BaseActivity<ShowAccountListActivityBinding> {
 
@@ -28,6 +30,28 @@ public class ShowAccountListActivity extends BaseActivity<ShowAccountListActivit
 
     private void clickEvent() {
 
+        clickAddMoneyImgButton();
+        clickAddMoneyTextButton();
+    }
+
+    private void clickAddMoneyImgButton() {
+
+        binding.addMoneyButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(getApplicationContext(), LookupSendMoneyActivity.class);
+
+            intent.putExtra("title_kind", binding.accountKindTitle.getText().toString());
+            intent.putExtra("account_money", binding.accountMoney.getText().toString());
+
+            startActivity(intent);
+        });
+    }
+
+    private void clickAddMoneyTextButton() {
+
+        binding.addMoneyButtonText.setOnClickListener(v -> {
+
+        });
     }
 
     @Override
