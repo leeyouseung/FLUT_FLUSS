064e117d9fe24f352353a7ec1875d0daabd7c92f
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-02 20:03:21 +0900

    Update UserRoom.java

--- app/src/main/java/com/example/flut_fluss/model/entity/UserRoom.java
+++ app/src/main/java/com/example/flut_fluss/model/entity/UserRoom.java
@@ -1,4 +1,15 @@
 package com.example.flut_fluss.model.entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user_table")
 public class UserRoom {
+
+    @PrimaryKey
+    private String id;
+
+    private String password;
+
+    private String username;
 }
