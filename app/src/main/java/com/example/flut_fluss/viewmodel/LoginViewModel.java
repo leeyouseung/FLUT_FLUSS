4cf784636b71852b9f8081e777b8632a99677229
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-03 17:41:33 +0900

    Create LoginViewModel.java

---
+++ app/src/main/java/com/example/flut_fluss/viewmodel/LoginViewModel.java
@@ -0,0 +1,15 @@
+package com.example.flut_fluss.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class LoginViewModel extends AndroidViewModel {
+
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+
+        
+    }
+}
