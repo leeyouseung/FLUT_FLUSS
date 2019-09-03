69ea728bde8de4924b918ab253a17146f7a8570d
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-03 09:08:41 +0900

    Update LoginApi.java

--- app/src/main/java/com/example/flut_fluss/network/api/LoginApi.java
+++ app/src/main/java/com/example/flut_fluss/network/api/LoginApi.java
@@ -1,18 +1,18 @@
-package com.example.flut_fluss.network.api;
-
-import androidx.annotation.NonNull;
-
-import com.example.flut_fluss.model.Login;
-import com.example.flut_fluss.network.request.LoginRequest;
-
-import io.reactivex.Single;
-import retrofit2.Response;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
-
-public interface LoginApi {
-
-    @NonNull
-    @POST("/auth/login")
-    Single<Response<Response<Login>>> login(@Body LoginRequest loginRequest);
-}
+//package com.example.flut_fluss.network.api;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.example.flut_fluss.model.Login;
+//import com.example.flut_fluss.network.request.LoginRequest;
+//
+//import io.reactivex.Single;
+//import retrofit2.Response;
+//import retrofit2.http.Body;
+//import retrofit2.http.POST;
+//
+//public interface LoginApi {
+//
+//    @NonNull
+//    @POST("/auth/login")
+//    Single<Response<Response<Login>>> login(@Body LoginRequest loginRequest);
+//}
