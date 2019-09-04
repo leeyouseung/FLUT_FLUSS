942d3420d097f308ceeccd564b6fe85f69e4f492
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-04 17:38:00 +0900

    Update Login.java

--- app/src/main/java/com/example/flut_fluss/model/Login.java
+++ app/src/main/java/com/example/flut_fluss/model/Login.java
@@ -1,9 +1,16 @@
 package com.example.flut_fluss.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Login {
 
+    @SerializedName("Token")
     private String token;
+
+    @SerializedName("RefreshToken")
     private String refreshToken;
+
+    @SerializedName("User")
     private User info;
 
     public String getToken() {
