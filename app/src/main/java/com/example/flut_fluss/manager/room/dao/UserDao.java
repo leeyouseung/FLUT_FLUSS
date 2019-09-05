987880da2e9687b54ab2d008cb2dbda75f078c79
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-05 16:24:28 +0900

    .

--- app/src/main/java/com/example/flut_fluss/manager/room/dao/UserDao.java
+++ app/src/main/java/com/example/flut_fluss/manager/room/dao/UserDao.java
@@ -1,28 +1,28 @@
-package com.example.flut_fluss.manager.room.dao;
-
-import androidx.lifecycle.LiveData;
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.Query;
-import androidx.room.Update;
-
-import com.example.flut_fluss.model.User;
-
-import java.util.List;
-
-@Dao
-public interface UserDao {
-
-    @Insert
-    void insert(User user);
-
-    @Update
-    void update(User user);
-
-    @Delete
-    void delete(User user);
-
-    @Query("SELECT * FROM user_table")
-    LiveData<List<User>> getUserInfo();
-}
+//package com.example.flut_fluss.manager.room.dao;
+//
+//import androidx.lifecycle.LiveData;
+//import androidx.room.Dao;
+//import androidx.room.Delete;
+//import androidx.room.Insert;
+//import androidx.room.Query;
+//import androidx.room.Update;
+//
+//import com.example.flut_fluss.model.User;
+//
+//import java.util.List;
+//
+//@Dao
+//public interface UserDao {
+//
+//    @Insert
+//    void insert(User user);
+//
+//    @Update
+//    void update(User user);
+//
+//    @Delete
+//    void delete(User user);
+//
+//    @Query("SELECT * FROM user_table")
+//    LiveData<List<User>> getUserInfo();
+//}
