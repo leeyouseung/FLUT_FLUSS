5f6ac8b0c52ff900a4ad94c04d1bc634c66c9f13
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-05 21:54:47 +0900

    Update Login.java

--- app/src/main/java/com/example/flut_fluss/model/Login.java
+++ app/src/main/java/com/example/flut_fluss/model/Login.java
@@ -7,23 +7,28 @@ public class Login {
     private User user;
 
     public Login(Token token, User user) {
+
         this.token = token;
         this.user = user;
     }
 
     public Token getToken() {
+
         return token;
     }
 
     public void setToken(Token token) {
+
         this.token = token;
     }
 
     public User getUser() {
+
         return user;
     }
 
     public void setUser(User user) {
+        
         this.user = user;
     }
 }
