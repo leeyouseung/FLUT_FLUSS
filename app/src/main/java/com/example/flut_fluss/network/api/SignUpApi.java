a38b35ebabcb84e2a2c807d453fdd60ef91908d1
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-06 20:27:59 +0900

    Update SignUpApi.java

--- app/src/main/java/com/example/flut_fluss/network/api/SignUpApi.java
+++ app/src/main/java/com/example/flut_fluss/network/api/SignUpApi.java
@@ -1,13 +1,13 @@
 package com.example.flut_fluss.network.api;
 
 import com.example.flut_fluss.network.request.SignUpRequest;
+import com.example.flut_fluss.network.Response;
 
 import io.reactivex.Single;
-import retrofit2.Response;
 import retrofit2.http.POST;
 
 public interface SignUpApi {
 
-    @POST("/api/user/sign")
-    Single<Response<com.example.flut_fluss.network.Response>> signup(SignUpRequest signUpRequest);
+    @POST("/api/user")
+    Single<retrofit2.Response<Response>> signup(SignUpRequest signUpRequest);
 }
