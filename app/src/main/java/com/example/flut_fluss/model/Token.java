b1174c790dda93115e499c1a567f6e1c49b361a8
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-06 10:10:59 +0900

    Update Token.java

--- app/src/main/java/com/example/flut_fluss/model/Token.java
+++ app/src/main/java/com/example/flut_fluss/model/Token.java
@@ -7,23 +7,28 @@ public class Token {
     private String refreshToken;
 
     public Token(String token, String refreshToken) {
+
         this.token = token;
         this.refreshToken = refreshToken;
     }
 
     public String getToken() {
+
         return token;
     }
 
     public void setToken(String token) {
+
         this.token = token;
     }
 
     public String getRefreshToken() {
+
         return refreshToken;
     }
 
     public void setRefreshToken(String refreshToken) {
+
         this.refreshToken = refreshToken;
     }
 }
