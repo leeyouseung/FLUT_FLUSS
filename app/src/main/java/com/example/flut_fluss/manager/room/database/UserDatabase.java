b9f65426696644814a7ff7fe02fdb4f57636d9b9
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-06 10:32:14 +0900

    set

--- app/src/main/java/com/example/flut_fluss/manager/room/database/UserDatabase.java
+++ app/src/main/java/com/example/flut_fluss/manager/room/database/UserDatabase.java
@@ -1,40 +1,40 @@
-package com.example.flut_fluss.manager.room.database;
-
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
-
-import com.example.flut_fluss.manager.room.dao.UserDao;
-import com.example.flut_fluss.manager.room.model.User;
-
-@Database(entities = {User.class}, version = 1)
-public abstract class UserDatabase extends RoomDatabase {
-
-    public static UserDatabase sInstance;
-    public abstract UserDao userDao();
-
-    public static synchronized UserDatabase getInstance(Context context) {
-
-        if(sInstance == null) {
-
-            sInstance = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, "user_database")
-                    .fallbackToDestructiveMigration()
-                    .addCallback(roomCallback)
-                    .build();
-        }
-
-        return sInstance;
-    }
-
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-
-        }
-    };
-}
+//package com.example.flut_fluss.manager.room.database;
+//
+//import android.content.Context;
+//
+//import androidx.annotation.NonNull;
+//import androidx.room.Database;
+//import androidx.room.Room;
+//import androidx.room.RoomDatabase;
+//import androidx.sqlite.db.SupportSQLiteDatabase;
+//
+//import com.example.flut_fluss.manager.room.dao.UserDao;
+//import com.example.flut_fluss.manager.room.model.User;
+//
+//@Database(entities = {User.class}, version = 1)
+//public abstract class UserDatabase extends RoomDatabase {
+//
+//    public static UserDatabase sInstance;
+//    public abstract UserDao userDao();
+//
+//    public static synchronized UserDatabase getInstance(Context context) {
+//
+//        if(sInstance == null) {
+//
+//            sInstance = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, "user_database")
+//                    .fallbackToDestructiveMigration()
+//                    .addCallback(roomCallback)
+//                    .build();
+//        }
+//
+//        return sInstance;
+//    }
+//
+//    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+//        @Override
+//        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+//            super.onCreate(db);
+//
+//        }
+//    };
+//}
--- app/src/main/java/com/example/flut_fluss/manager/room/model/User.java
+++ app/src/main/java/com/example/flut_fluss/manager/room/model/User.java
@@ -1,98 +1,98 @@
-package com.example.flut_fluss.manager.room.model;
-
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-
-@Entity(tableName = "user_table")
-public class User {
-
-    @PrimaryKey
-    @NonNull
-    private String id;
-
-    private String password;
-
-    private String name;
-
-    private String phoneNumber;
-
-    private String bankAccount;
-
-    private String flutAccount;
-
-    public User() {
-
-    }
-
-    public User(String id, String password, String name, String phoneNumber, String bankAccount, String flutAccount) {
-
-        this.id = id;
-        this.password = password;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.bankAccount = bankAccount;
-        this.flutAccount = flutAccount;
-    }
-
-    public String getId() {
-
-        return id;
-    }
-
-    public void setId(String id) {
-
-        this.id = id;
-    }
-
-    public String getPassword() {
-
-        return password;
-    }
-
-    public void setPassword(String password) {
-
-        this.password = password;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-
-
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getBankAccount() {
-
-        return bankAccount;
-    }
-
-    public void setBankAccount(String bankAccount) {
-
-        this.bankAccount = bankAccount;
-    }
-
-    public String getFlutAccount() {
-
-        return flutAccount;
-    }
-
-    public void setFlutAccount(String flutAccount) {
-
-        this.flutAccount = flutAccount;
-    }
-}
+//package com.example.flut_fluss.manager.room.model;
+//
+//import androidx.annotation.NonNull;
+//import androidx.room.Entity;
+//import androidx.room.PrimaryKey;
+//
+//@Entity(tableName = "user_table")
+//public class User {
+//
+//    @PrimaryKey
+//    @NonNull
+//    private String id;
+//
+//    private String password;
+//
+//    private String name;
+//
+//    private String phoneNumber;
+//
+//    private String bankAccount;
+//
+//    private String flutAccount;
+//
+//    public User() {
+//
+//    }
+//
+//    public User(String id, String password, String name, String phoneNumber, String bankAccount, String flutAccount) {
+//
+//        this.id = id;
+//        this.password = password;
+//        this.name = name;
+//        this.phoneNumber = phoneNumber;
+//        this.bankAccount = bankAccount;
+//        this.flutAccount = flutAccount;
+//    }
+//
+//    public String getId() {
+//
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//
+//        this.id = id;
+//    }
+//
+//    public String getPassword() {
+//
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//
+//        this.password = password;
+//    }
+//
+//    public String getName() {
+//
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//
+//        this.name = name;
+//    }
+//
+//    public String getPhoneNumber() {
+//
+//
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getBankAccount() {
+//
+//        return bankAccount;
+//    }
+//
+//    public void setBankAccount(String bankAccount) {
+//
+//        this.bankAccount = bankAccount;
+//    }
+//
+//    public String getFlutAccount() {
+//
+//        return flutAccount;
+//    }
+//
+//    public void setFlutAccount(String flutAccount) {
+//
+//        this.flutAccount = flutAccount;
+//    }
+//}
