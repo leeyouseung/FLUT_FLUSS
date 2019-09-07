40fbad6b4c947d3d52ad0514b2790240154d8ad8
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-07 14:46:00 +0900

    Update Money.java

--- app/src/main/java/com/example/flut_fluss/model/Money.java
+++ app/src/main/java/com/example/flut_fluss/model/Money.java
@@ -1,4 +1,21 @@
 package com.example.flut_fluss.model;
 
 public class Money {
+
+    private String money;
+
+    public Money(String money) {
+
+        this.money = money;
+    }
+
+    public String getMoney() {
+
+        return money;
+    }
+
+    public void setMoney(String money) {
+        
+        this.money = money;
+    }
 }
