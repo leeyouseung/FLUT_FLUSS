757bea084a1e696c74ddc8d8d03bd7468eb2ffc5
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-08 20:42:21 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/flut_fluss/view/activity/main/MainActivity.java
+++ app/src/main/java/com/example/flut_fluss/view/activity/main/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
