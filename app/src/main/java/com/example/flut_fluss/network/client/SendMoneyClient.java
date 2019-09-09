3a0efcfc1bfd783a55df4269f527f006c4305885
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-09 11:14:45 +0900

    Update SendMoneyClient.java

--- app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
+++ app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
@@ -1,7 +1,11 @@
 package com.example.flut_fluss.network.client;
 
 import com.example.flut_fluss.base.BaseClient;
+import com.example.flut_fluss.model.Money;
 import com.example.flut_fluss.network.api.SendMoneyApi;
+import com.example.flut_fluss.network.request.SendMoneyRequest;
+
+import io.reactivex.Single;
 
 public class SendMoneyClient extends BaseClient<SendMoneyApi> {
 
@@ -10,4 +14,9 @@ public class SendMoneyClient extends BaseClient<SendMoneyApi> {
 
         return SendMoneyApi.class;
     }
+
+//    public Single<Money> sendMoney(SendMoneyRequest sendMoneyRequest) {
+    
+//        return api
+//    }
 }
