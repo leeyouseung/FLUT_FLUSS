fe480fa97c56b27fa8e155e2c98043a9f689dc23
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-09 10:39:23 +0900

    Update SendMoneyClient.java

--- app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
+++ app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
@@ -1,12 +1,13 @@
 package com.example.flut_fluss.network.client;
 
 import com.example.flut_fluss.base.BaseClient;
+import com.example.flut_fluss.network.api.SendMoneyApi;
 
-public class SendMoneyClient extends BaseClient {
+public class SendMoneyClient extends BaseClient<SendMoneyApi> {
 
     @Override
     protected Class api() {
 
-        return null;
+        return SendMoneyApi.class;
     }
 }
