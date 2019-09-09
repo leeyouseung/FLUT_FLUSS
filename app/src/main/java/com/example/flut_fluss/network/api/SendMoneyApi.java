39fca78fb0c6c97e403f092633e2cdcd396f4430
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-09 13:48:58 +0900

    set

--- app/src/main/java/com/example/flut_fluss/network/api/SendMoneyApi.java
+++ app/src/main/java/com/example/flut_fluss/network/api/SendMoneyApi.java
@@ -2,12 +2,13 @@ package com.example.flut_fluss.network.api;
 
 import com.example.flut_fluss.model.Login;
 import com.example.flut_fluss.network.Response;
+import com.example.flut_fluss.network.request.SendMoneyRequest;
 
 import io.reactivex.Single;
 import retrofit2.http.POST;
 
 public interface SendMoneyApi {
 
-//    @POST("")
-//    Single<retrofit2.Response<Response>> sendMoney(SendMoneyRequest sendMoneyRequest);
+    @POST("")
+    Single<retrofit2.Response<Response>> sendMoney(SendMoneyRequest sendMoneyRequest);
 }
--- app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
+++ app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
@@ -1,9 +1,14 @@
 package com.example.flut_fluss.network.client;
 
 import com.example.flut_fluss.base.BaseClient;
+import com.example.flut_fluss.model.Login;
 import com.example.flut_fluss.model.Money;
 import com.example.flut_fluss.network.api.SendMoneyApi;
 import com.example.flut_fluss.network.request.SendMoneyRequest;
+
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 import io.reactivex.Single;
 
@@ -16,7 +21,30 @@ public class SendMoneyClient extends BaseClient<SendMoneyApi> {
     }
 
 //    public Single<Money> sendMoney(SendMoneyRequest sendMoneyRequest) {
-    
-//        return api
+
+//        return api.sendMoney(sendMoneyRequest).map(response -> {
+//
+//            if (!response.isSuccessful()) {
+//
+//                JSONObject errorBody = new JSONObject(Objects
+//                        .requireNonNull(
+//                                response.errorBody()).string());
+//
+//                throw new Exception(errorBody.getString("message"));
+//            }
+//
+//            if (response.body().getStatus() == 200) {
+//
+//                return response.body().getMessage();
+//            }
+//            else if (response.body().getStatus() == 400) {
+//
+//                throw new Exception("아이디가 중복되었습니다.");
+//            }
+//            else {
+//
+//                throw new Exception(response.body().getMessage());
+//            }
+//        });
 //    }
 }
