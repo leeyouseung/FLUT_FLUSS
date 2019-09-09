6ff7911aec2685b67b600c7e01456d352ef0ecbe
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-09 16:43:43 +0900

    creat find pw xml java

---
+++ app/src/main/java/com/example/flut_fluss/view/activity/find/FindPasswordActivity.java
@@ -0,0 +1,44 @@
+package com.example.flut_fluss.view.activity.find;
+
+import android.os.Bundle;
+
+import com.example.flut_fluss.R;
+import com.example.flut_fluss.base.BaseActivity;
+import com.example.flut_fluss.databinding.FindPasswordActivityBinding;
+
+public class FindPasswordActivity extends BaseActivity<FindPasswordActivityBinding> {
+
+    @Override
+    protected int layoutId() {
+
+        return R.layout.find_password_activity;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initViewModel();
+
+        initData();
+
+        event();
+    }
+
+    private void initViewModel() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void event() {
+
+        clickEvent();
+    }
+
+    private void clickEvent() {
+        
+    }
+}
