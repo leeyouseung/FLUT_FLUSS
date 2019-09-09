a44cac7ace463caed193699a342c6d0c8dbf6601
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-09 16:41:56 +0900

    create find id activity

---
+++ app/src/main/java/com/example/flut_fluss/view/activity/find/FindIdActivity.java
@@ -0,0 +1,44 @@
+package com.example.flut_fluss.view.activity.find;
+
+import android.os.Bundle;
+
+import com.example.flut_fluss.R;
+import com.example.flut_fluss.base.BaseActivity;
+import com.example.flut_fluss.databinding.FindIdActivityBinding;
+
+public class FindIdActivity extends BaseActivity<FindIdActivityBinding> {
+
+    @Override
+    protected int layoutId() {
+
+        return R.layout.find_id_activity;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initViewModel();
+
+        initData();
+
+        event();
+    }
+
+    private void initViewModel() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void event() {
+
+        clickEvent();
+    }
+
+    private void clickEvent() {
+        
+    }
+}
