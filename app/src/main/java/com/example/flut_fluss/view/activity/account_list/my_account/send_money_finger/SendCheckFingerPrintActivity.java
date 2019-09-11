99dd65c4c58700494157b76417c3bd4c8378d8cf
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-11 23:52:59 +0900

    create send_check_finger.xml

---
+++ app/src/main/java/com/example/flut_fluss/view/activity/account_list/my_account/send_money_finger/SendCheckFingerPrintActivity.java
@@ -0,0 +1,44 @@
+package com.example.flut_fluss.view.activity.account_list.my_account.send_money_finger;
+
+import android.os.Bundle;
+
+import com.example.flut_fluss.R;
+import com.example.flut_fluss.base.BaseActivity;
+import com.example.flut_fluss.databinding.SendCheckFingerPrintActivityBinding;
+
+public class SendCheckFingerPrintActivity extends BaseActivity<SendCheckFingerPrintActivityBinding> {
+
+    @Override
+    protected int layoutId() {
+
+        return R.layout.send_check_finger_print_activity;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initViewModel();
+
+        initData();
+
+        event();
+    }
+
+    private void initViewModel() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void event() {
+
+        clickEvent();
+    }
+
+    private void clickEvent() {
+
+    }
+}
