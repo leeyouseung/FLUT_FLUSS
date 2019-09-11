49d362395ae7bc184222e64430534d69f31c5933
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-11 21:37:51 +0900

    Update FingerprintHandler.java

--- app/src/main/java/com/example/flut_fluss/manager/lock/FingerprintHandler.java
+++ app/src/main/java/com/example/flut_fluss/manager/lock/FingerprintHandler.java
@@ -1,21 +1,30 @@
 package com.example.flut_fluss.manager.lock;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.CancellationSignal;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.flut_fluss.R;
-import com.example.flut_fluss.databinding.FingerPrintActivityBinding;
 import com.example.flut_fluss.view.activity.main.MainActivity;
 
+@TargetApi(Build.VERSION_CODES.N)
 public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
 
-    private FingerPrintActivityBinding binding;
+    CancellationSignal cancellationSignal;
 
     private Context context;
 
@@ -36,18 +45,67 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         fingerprintManager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
     }
 
+    //------------------------
+
+    @Override
+    public void onAuthenticationError(int errorCode, CharSequence errString) {
+
+        this.update("인증 에러 발생" + errString, false);
+    }
+
     @Override
     public void onAuthenticationFailed() {
-        super.onAuthenticationFailed();
 
-        binding.tvMessage.setTextColor(context.getResources().getColor(R.color.red));
-        binding.tvMessage.setText("지문 인식에 실패 했습니다.");
+        this.update("인증 실패", false);
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+
+        this.update("Error : "+ helpString, false);
     }
 
     @Override
     public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
         super.onAuthenticationSucceeded(result);
 
+        this.update("앱 접근이 허용되었습니다.", true);
+
         context.startActivity(new Intent(context, MainActivity.class));
+    }
+
+    public void stopFingerAuth() {
+
+        if (cancellationSignal != null && !cancellationSignal.isCanceled()) {
+
+            cancellationSignal.cancel();
+        }
+    }
+
+    private void update(String s, boolean b) {
+
+        final TextView tv_message = ((Activity)context).findViewById(R.id.tv_message);
+        final ImageView iv_fingerprint = ((Activity)context).findViewById(R.id.iv_fingerprint);
+
+        //안내 메세지 출력
+        tv_message.setText(s);
+
+        if (!b) {
+
+            tv_message.setTextColor(ContextCompat.getColor(context, R.color.red));
+        }
+        else {
+
+            //지문인증 성공
+            tv_message.setTextColor(ContextCompat.getColor(context, R.color.login_button_first_gradation));
+            iv_fingerprint.setImageResource(R.drawable.finger_print_done_icon);
+
+            /*
+            //sound effect
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone((Activity)context, notification);
+            r.play();
+             */
+        }
     }
 }
