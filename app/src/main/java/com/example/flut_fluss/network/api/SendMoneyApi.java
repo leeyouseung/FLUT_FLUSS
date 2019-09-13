7968c2cca34b103e663f031d9d165264b75e1872
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-13 13:59:54 +0900

    Update SendMoneyApi.java

--- app/src/main/java/com/example/flut_fluss/network/api/SendMoneyApi.java
+++ app/src/main/java/com/example/flut_fluss/network/api/SendMoneyApi.java
@@ -9,6 +9,6 @@ import retrofit2.http.POST;
 
 public interface SendMoneyApi {
 
-    @POST("api/")
+    @POST("api/#")
     Single<retrofit2.Response<Response>> sendMoney(@Body SendMoneyRequest sendMoneyRequest);
 }
