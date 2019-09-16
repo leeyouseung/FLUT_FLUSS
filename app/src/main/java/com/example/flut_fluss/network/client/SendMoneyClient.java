0764a4ea5ed72383bcea34e989050d6670cc509c
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-09-16 17:38:04 +0900

    Update SendMoneyClient.java

--- app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
+++ app/src/main/java/com/example/flut_fluss/network/client/SendMoneyClient.java
@@ -42,7 +42,7 @@ public class SendMoneyClient extends BaseClient<SendMoneyApi> {
             } else if (response.body().getStatus() == 400) {
 
                 throw new Exception("송금 오류");
-                
+
             } else {
 
                 throw new Exception(response.body().getMessage());
